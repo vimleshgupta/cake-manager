51259bbcc6beb6476d7347688c44b3e2632607b8
vimleshgupta <v.gupta@reply.com>
2022-01-30 23:37:02 +0000

    Initial commit

---
+++ cake-manager-service/src/main/java/com/waracle/cakemanagerservice/CakeManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.waracle.cakemanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeManagerServiceApplication.class, args);
+	}
+
+}
---
+++ cake-manager-service/src/test/java/com/waracle/cakemanagerservice/CakeManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waracle.cakemanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
