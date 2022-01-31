bfc37f67ffeafd1f210ac468caec545bbd93641c
vimleshgupta <v.gupta@reply.com>
2022-01-31 16:22:02 +0000

    Add docker compose file for backend and frontend applications

--- cake-manager-service/src/main/java/com/waracle/cakemanagerservice/CakeManagerServiceApplication.java
+++ cake-manager-service/src/main/java/com/waracle/cakemanagerservice/CakeManagerServiceApplication.java
@@ -35,7 +35,7 @@ public class CakeManagerServiceApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost","http://localhost:80"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
